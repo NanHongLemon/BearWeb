1937eb7970cfcf0a4c58b35b30fc7120143156d9
qoqo5555 <qoqo5555@gmail.com>
2020-06-19 09:56:29 +0800

    add home.

--- src/main/java/com/web/bear/action/IndexAction.java
+++ src/main/java/com/web/bear/action/IndexAction.java
@@ -3,11 +3,18 @@ package com.web.bear.action;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexAction {
 
-    @GetMapping("index")
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Hello World.");
+        return "index";
+    }
+
+    @GetMapping("/index")
     public String addMemberPage(Model model) {
 
         model.addAttribute("message", "First apps.");
